fbc36c7de7544177ca9278da2055dbbdaab3b95c
Anastasiya <33719990+Anastasiya199889911@users.noreply.github.com>
2019-01-15 22:14:38 +0300

    вывод преподавателей на главной странице

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const app = require('./app')
 const port = process.env.port || 3000
 const News = require('./models/news')
+const Teachers=require('./models/teachers')
 
 
 
 
 app.get("/", async(req, res)=>{
+    // вывод новостей
     const count_news= await News.find({}).count()
-    const news = await News.find({}).skip(count_news-3)
+    let news={}
+    if(count_news<=3)
+    {
+        news = await News.find({})
+    }
+    else
+    {
+        news = await News.find({}).skip(count_news-3)
+    }
     news.reverse()
-    res.render('index.njk',{news});
+    // вывод преподавателей
+    const teachers = await Teachers.find({})
+    res.render('index.njk',{news,teachers});
 })
 
 app.get("/news", async(req, res)=>{
@@ -18,15 +30,31 @@ app.get("/news", async(req, res)=>{
     res.render('news.njk',{news});
 })
 // добавление новости
-app.get('/get', async(req,res)=>{
+app.get('/addNews', async(req,res)=>{
     const news = await new News({
         linkPicture: 'img/uploads/file1.png',
-        title: 'Новость №4',
-        text: 'Текст новости №4'
+        title: 'Новость №5',
+        text: 'Текст новости №5'
     })
     try {
         news.save()
         console.log('Новость добавлен')
+    }
+    catch (e) {
+        console.log(e)
+    }
+    res.status(200).json({ st:"Your order is accepted"})
+})
+// добавление преподавателей
+app.get('/addTeachers', async(req,res)=>{
+    const teachers = await new Teachers({
+        linkPicture: 'img/uploads/teacher1.png',
+        name: 'Скрипник Маргарита Викторовна',
+        description: 'Закончила Харьковский Национальный Медицинский Университет.'
+    })
+    try {
+        teachers.save()
+        console.log('Преподаватель добавлен')
     }
     catch (e) {
         console.log(e)
